eb2d880147c44d33882fb26d597a17221296b5fa
Jo <loluvulol@gmail.com>
2022-10-28 15:54:13 +1000

    Private variable

--- classes.cpp
+++ classes.cpp
@@ -11,44 +11,52 @@ enum Gender
 
 class Person
 {
+//Not accessible outside the class
+private:
+    int height;
+
 public:
     string name;
     int age;
     Gender gender;
 
-
-    //Constructor to initialize a new object
-    Person(string _name, int _age, Gender _gender){
+    // Constructor to initialize a new object
+    Person(string _name, int _age, Gender _gender, int _height)
+    {
         name = _name;
         age = _age;
         gender = _gender;
+        height = _height;
     };
 
-    //Constructor with no arguments (set default values)
-    Person(){
+    // Constructor with no arguments (set default values)
+    Person()
+    {
         name = "???";
         age = 99;
         gender = nonBinary;
+        height = 0;
     };
 
-    //Methods are only in classes, not structs
-    void printInfo() {
-        cout << name << " " << age << " " << gender << endl;
+    // Methods are only in classes, not structs
+    void printInfo()
+    {
+        cout << name << " " << age << " " << gender << " " << height << endl;
     }
 };
 
 int main()
 {
 
-    Person jo("Jo", 28, female);
+    Person jo("Jo", 28, female, 160);
 
-    Person lorenzo("Lorenzo", 22, male);
+    Person lorenzo("Lorenzo", 22, male, 200);
     Person noName;
 
-    //Output is the same as Structs
+    // Output is the same as Structs
     jo.printInfo();
     lorenzo.printInfo();
-    
+
     noName.printInfo();
 
     return 0;
