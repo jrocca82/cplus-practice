201f5b3d287482acc49106f63bc109aec1c4924e
Jo <loluvulol@gmail.com>
2022-10-28 15:43:51 +1000

    Constructors

--- classes.cpp
+++ classes.cpp
@@ -16,6 +16,21 @@ public:
     int age;
     Gender gender;
 
+
+    //Constructor to initialize a new object
+    Person(string _name, int _age, Gender _gender){
+        name = _name;
+        age = _age;
+        gender = _gender;
+    };
+
+    //Constructor with no arguments (set default values)
+    Person(){
+        name = "???";
+        age = 99;
+        gender = nonBinary;
+    };
+
     //Methods are only in classes, not structs
     void printInfo() {
         cout << name << " " << age << " " << gender << endl;
@@ -25,19 +40,16 @@ public:
 int main()
 {
 
-    Person jo;
-    jo.name = "Jo";
-    jo.age = 28;
-    jo.gender = female;
+    Person jo("Jo", 28, female);
 
-    Person lorenzo;
-    lorenzo.name = "Lorenzo";
-    lorenzo.age = 22;
-    lorenzo.gender = male;
+    Person lorenzo("Lorenzo", 22, male);
+    Person noName;
 
     //Output is the same as Structs
     jo.printInfo();
     lorenzo.printInfo();
+    
+    noName.printInfo();
 
     return 0;
 }
